fa9b1afd771634d932350d3db5a9951eaa9aa185
Zimu Liu <zimu.liu@gmail.com>
2016-01-20 22:40:04 -0500

    Backport Rails 4.2 support
    https://github.com/mbleigh/acts-as-taggable-on/commit/f7fe2e4a2962baa625f6a40af008d78654a4cc23

--- lib/acts_as_taggable_on/acts_as_taggable_on/core.rb
+++ lib/acts_as_taggable_on/acts_as_taggable_on/core.rb
@@ -314,10 +314,10 @@ module ActsAsTaggableOn::Taggable
         if changed_attributes.include?(attrib)
           # The attribute already has an unsaved change.
           old = changed_attributes[attrib]
-          changed_attributes.delete(attrib) if (old.to_s == value.to_s)
+          @changed_attributes.delete(attrib) if (old.to_s == value.to_s)
         else
           old = tag_list_on(context).to_s
-          changed_attributes[attrib] = old if (old.to_s != value.to_s)
+          @changed_attributes[attrib] = old if (old.to_s != value.to_s)
         end
       end
 
@@ -389,6 +389,20 @@ module ActsAsTaggableOn::Taggable
         end
 
         true
+      end
+
+      private
+
+      # Filters the tag lists from the attribute names.
+      def attributes_for_update(attribute_names)
+        tag_lists = tag_types.map {|tags_type| "#{tags_type.to_s.singularize}_list"}
+        super.delete_if {|attr| tag_lists.include? attr }
+      end
+
+      # Filters the tag lists from the attribute names.
+      def attributes_for_create(attribute_names)
+        tag_lists = tag_types.map {|tags_type| "#{tags_type.to_s.singularize}_list"}
+        super.delete_if {|attr| tag_lists.include? attr }
       end
     end
   end
